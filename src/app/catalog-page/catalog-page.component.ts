2437c5f153d0f830438e7c4b6ebbccad712dcb13
Enricods-i <e.desalve@outlook.com>
2022-09-19 16:22:21 +0200

    Sistemato l'aggiungi al carrello mentre si visualizzano i fumetti

--- src/app/catalog-page/catalog-page.component.ts
+++ src/app/catalog-page/catalog-page.component.ts
@@ -1,7 +1,14 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CartService } from '../cart.service';
 import { ComicService } from '../comic.service';
+import { ProblemCode } from '../common/ProblemCode';
+import { Cart } from '../model/Cart';
 import { Collection } from '../model/Collection';
 import { Comic } from '../model/Comic';
+import { User } from '../model/User';
+import { SessionService } from '../session.service';
 
 @Component({
   selector: 'app-catalog-page',
@@ -10,18 +17,36 @@ import { Comic } from '../model/Comic';
 })
 export class CatalogPageComponent {
 
+  user?: User;
+  cart?: Cart;
+
   collection?: Collection;
-  comics: Comic[] = [];
 
   currentPage: number = 1;
+  comics: Comic[] = [];
 
-  constructor(private comicService: ComicService) {
+  constructor(
+    private comicService: ComicService,
+    private sessionService: SessionService,
+    private cartService: CartService,
+    private snackBar: MatSnackBar
+  )
+  {
+    this.sessionService.currentUser.subscribe(user => this.user = user);
+    this.sessionService.currentCart.subscribe(cart => this.cart = cart);
     this.collection = history.state.collection;
     this.getComics();
   }
 
+  showMessage(msg: string){
+    this.snackBar.open(msg, undefined, {duration: 5000});
+  }
+
   getComics(){
-    if (this.collection == undefined) return;
+    if (this.collection == undefined){
+      this.showMessage("Errore interno");
+      return;
+    }
     this.comicService.getByCollection(this.collection.id, this.currentPage-1).subscribe({
       next: (response: Comic[]) => {
         this.comics = response;
@@ -39,12 +64,28 @@ export class CatalogPageComponent {
     this.getComics();
   }
 
-  increase(s: string): string{
-    return String(parseInt(s)+1);
-  }
-
-  decrease(s: string): string{
-    return String(parseInt(s)-1);
+  addToCart(c: Comic, q: number){
+    if (this.user == undefined){
+      this.showMessage("Errore interno");
+      return;
+    }
+    this.cartService.addComic(this.user.id, c.id, q).subscribe({
+      next: () => {
+        if(this.cart == undefined) return;
+        this.cart.content.push({
+          quantity: q,
+          comic: c
+        });
+        this.cart.size+=q;
+      },
+      error: (problem: HttpErrorResponse) => {
+        if(problem.error[0].code == ProblemCode.COMIC_QUANTITY_UNAVAIABLE) this.showMessage("Quantità non disponibile");
+        else if(problem.error[0].code == ProblemCode.COMIC_ALREADY_IN_CART){
+          this.showMessage("Fumetto già nel carrello")
+        }
+        else console.error(problem.error[0]);
+      }
+    });
   }
 
 }
